621b3bdaff99bb7152ab176a1337eb77e784349a
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-10-16 17:36:33 +0500

    chore: fix cicd

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const withNextra = nextra({
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default withNextra(nextConfig);
